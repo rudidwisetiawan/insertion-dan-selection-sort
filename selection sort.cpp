745a001da4cca67ff614c4496003b853661b6369
RUDI DWI SETIAWAN <47816046+rudidwisetiawan@users.noreply.github.com>
2019-03-28 14:16:28 +0700

    Add files via upload

---
+++ selection sort.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,temp,arr[100];
+            cout<<"MASUKKAN JUMLAH ANGKA : ";
+            cin>>b;
+    for(int j=0;j<b;j++)
+        {
+
+            cout<<"masukan angka ke- " <<j+1<<" : ";
+            cin >> arr[j];
+        }
+
+    for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<4;j++)
+        {
+
+            for(int i=0;i<b;i++)
+                {
+            for(int j=0;j<b-1;j++)
+                {
+            if(arr[i]<arr[j])
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+
+            for(a=0;a<5;a++)
+                {
+            for(a=0;a<b;a++)
+            {
+                cout<<endl;
+                cout<<arr[a];
+
+    }
+    }
+        }
+    }
+}
